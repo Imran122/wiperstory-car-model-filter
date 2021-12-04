b63a0a46cc55a8a13ead91f4ce992841f4ee181b
ghost <imransardar122@gmail.com>
2021-12-04 21:30:40 +0600

    added service part

--- src/components/Home/Services/Service.js
+++ src/components/Home/Services/Service.js
@@ -4,53 +4,53 @@ import { Icon } from '@iconify/react';
 const Service = () => {
     return (
         <>
-            <section id="services" class="services">
-                <div class="container">
+            <section id="services" className="services">
+                <div className="container">
 
-                    <div class="section-title">
+                    <div className="section-title">
                         <h2>Services</h2>
                         <h3>We do offer awesome <span>Services</span></h3>
                         <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
                     </div>
 
-                    <div class="row">
-                        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-                            <div class="icon-box">
-                                <div class="icon">
+                    <div className="row">
+                        <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+                            <div className="icon-box">
+                                <div className="icon">
                                     <Icon icon="mdi:account-child-circle" width="50" height="50" />
                                 </div>
-                                <h4 class="title"><a href="">Lorem Ipsum</a></h4>
-                                <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+                                <h4 className="title"><a href="">Lorem Ipsum</a></h4>
+                                <p className="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
                             </div>
                         </div>
 
-                        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-                            <div class="icon-box">
-                                <div class="icon">
+                        <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+                            <div className="icon-box">
+                                <div className="icon">
                                     <Icon icon="mdi:account-child-circle" width="50" height="50" />
                                 </div>
-                                <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-                                <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+                                <h4 className="title"><a href="">Sed ut perspiciatis</a></h4>
+                                <p className="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
                             </div>
                         </div>
 
-                        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-                            <div class="icon-box">
-                                <div class="icon">
+                        <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+                            <div className="icon-box">
+                                <div className="icon">
                                     <Icon icon="mdi:account-child-circle" width="50" height="50" />
                                 </div>
-                                <h4 class="title"><a href="">Magni Dolores</a></h4>
-                                <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+                                <h4 className="title"><a href="">Magni Dolores</a></h4>
+                                <p className="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
                             </div>
                         </div>
 
-                        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-                            <div class="icon-box">
-                                <div class="icon">
+                        <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+                            <div className="icon-box">
+                                <div className="icon">
                                     <Icon icon="mdi:account-child-circle" width="50" height="50" />
                                 </div>
-                                <h4 class="title"><a href="">Nemo Enim</a></h4>
-                                <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+                                <h4 className="title"><a href="">Nemo Enim</a></h4>
+                                <p className="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
                             </div>
                         </div>
 
--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -8,19 +8,19 @@ const Footer = () => {
 
 
 
-            <div class="footer">
-                <div class="inner-footer">
+            <div className="footer">
+                <div className="inner-footer">
 
 
-                    <div class="footer-items">
+                    <div className="footer-items">
                         <h1>Company Name</h1>
                         <p>Description of your company product company.</p>
                     </div>
 
 
-                    <div class="footer-items">
+                    <div className="footer-items">
                         <h3>Quick Links</h3>
-                        <div class="border1"></div>
+                        <div className="border1"></div>
                         <ul>
                             <a href="#"><li>Home</li></a>
                             <a href="#"><li>Search</li></a>
@@ -30,9 +30,9 @@ const Footer = () => {
                     </div>
 
 
-                    <div class="footer-items">
+                    <div className="footer-items">
                         <h3>Recipes</h3>
-                        <div class="border1"></div>
+                        <div className="border1"></div>
                         <ul>
                             <a href="#"><li>Indian</li></a>
                             <a href="#"><li>Chinese</li></a>
@@ -42,26 +42,26 @@ const Footer = () => {
                     </div>
 
 
-                    <div class="footer-items">
+                    <div className="footer-items">
                         <h3>Contact us</h3>
-                        <div class="border1"></div>
+                        <div className="border1"></div>
                         <ul>
-                            <li><i class="fa fa-map-marker" aria-hidden="true"></i>XYZ, abc</li>
-                            <li><i class="fa fa-phone" aria-hidden="true"></i>123456789</li>
-                            <li><i class="fa fa-envelope" aria-hidden="true"></i>xyz@gmail.com</li>
+                            <li><i className="fa fa-map-marker" aria-hidden="true"></i>XYZ, abc</li>
+                            <li><i className="fa fa-phone" aria-hidden="true"></i>123456789</li>
+                            <li><i className="fa fa-envelope" aria-hidden="true"></i>xyz@gmail.com</li>
                         </ul>
 
 
-                        <div class="social-media">
-                            <a href="#"><i class="fab fa-instagram"></i></a>
-                            <a href="#"><i class="fab fa-facebook"></i></a>
-                            <a href="#"><i class="fab fa-google-plus-square"></i></a>
+                        <div className="social-media">
+                            <a href="#"><i className="fab fa-instagram"></i></a>
+                            <a href="#"><i className="fab fa-facebook"></i></a>
+                            <a href="#"><i className="fab fa-google-plus-square"></i></a>
                         </div>
                     </div>
                 </div>
 
 
-                <div class="footer-bottom">
+                <div className="footer-bottom">
                     Copyright &copy; wiper story 2021.
                 </div>
             </div>
