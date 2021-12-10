89e1a11ac9dde8b9aba668bfb59509689c71c990
ghost <imransardar122@gmail.com>
2021-12-10 13:32:37 +0600

    meta tag2

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -3,10 +3,16 @@ import Advertise from '../Advertise/Advertise';
 import Banner from '../Banner/Banner';
 import CarForm from '../CarForm/CarForm';
 import Service from '../Services/Service';
-
+import { Helmet } from 'react-helmet';
 const Home = () => {
     return (
         <div>
+            <Helmet>‍
+                <title>wiper story</title>
+                <meta name="description" content="wiper story" />
+                <meta property="og:title" content="wiper story" />
+                <meta property="og:description" content="wiper story" />
+            </Helmet>
             <Banner></Banner>
             <Advertise></Advertise>
             <CarForm></CarForm>
